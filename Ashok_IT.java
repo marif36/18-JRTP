1efb8c30d6cff6606ae8304e7d8c9799ff2dac39
Arif <mohd.arif1236@gmail.com>
2021-11-12 00:16:20 +0530

    Initial commit

--- Ashok_IT.java
+++ Ashok_IT.java
@@ -3,6 +3,7 @@ package in.arifer.beans;
 public class Ashok_IT {
 	public static void main(String[] args) {
 		System.out.println("***Ashok-it<=Ameerpet=>Champ***");
+                System.out.println("18-JRTP...Running Batch");
 	}
 
 }
